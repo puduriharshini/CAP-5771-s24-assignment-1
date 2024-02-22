18a3197979e05c53790658f090c94ad099ec589d
puduriharshini <158066069+puduriharshini@users.noreply.github.com>
2024-02-22 12:08:41 -0500

    part_3_template_solution.py

--- part_3_template_solution.py
+++ part_3_template_solution.py
@@ -142,28 +142,17 @@ class Section3:
     B. Repeat part 1.B but return an imbalanced dataset consisting of 90% of all 9s removed.  Also convert the 7s to 0s and 9s to 1s.
     """
 
-    def partB(
+     def partB(
         self,
-        X: NDArray[np.floating],
-        y: NDArray[np.int32],
-        Xtest: NDArray[np.floating],
-        ytest: NDArray[np.int32],
-    ) -> tuple[
-        dict[Any, Any],
-        NDArray[np.floating],
-        NDArray[np.int32],
-        NDArray[np.floating],
-        NDArray[np.int32],
-    ]:
-        """"""
-        # Enter your code and fill the `answer` dictionary
-        answer = {}
-        print("Part 3B")
-        #X, y, Xtest, ytest = u.prepare_data()
-        Xtrain, ytrain = X, y
-        Xtest, ytest = Xtest, ytest
+    ):
+        print("Part 1B")
+        X, y, Xtest, ytest = u.prepare_data() # preparing initial dataset
+        Xtrain, ytrain = u.filter_out_7_9s(X, y)
+        Xtest, ytest = u.filter_out_7_9s(Xtest, ytest)
         test_Xtrain = nu.scalex(Xtrain)
         test_Xtest = nu.scalex(Xtest)
+        
+        # to ensure that our labels are integers, we test them
         test_ytrain = nu.scaley(ytrain)
         test_ytest = nu.scaley(ytest)
         print("Xtrain elements scaled from 0 to 1 and represented as floating point numbers? " +str(test_Xtrain))
@@ -172,25 +161,24 @@ class Section3:
         print("ytest elements represented as integers? " +str(test_ytest))
         answer = {}
 
-        length_Xtrain = len(Xtrain)
-        length_Xtest = len(Xtest)
-        length_ytrain = len(ytrain)
-        length_ytest = len(ytest)
+        # Enter your code and fill the `answer` dictionary
+
+        len_Xtrain = len(Xtrain)
+        len_Xtest = len(Xtest)
+        len_Ytrain = len(ytrain)
+        len_Ytest = len(ytest)
         max_Xtrain = Xtrain.max()
         max_Xtest = Xtest.max()
-        print(f"3B - The lengths of Xtrain, Xtest, ytrain, ytest is: {length_Xtrain}, {length_Xtest}, {length_ytrain}, {length_ytest}")
-        print(f"3B - The maximum value of Xtrain and Xtest is: {max_Xtrain}, {max_Xtest}")
-        answer["length_Xtrain"] = length_Xtrain 
-        answer["length_Xtest"] = length_Xtest
-        answer["length_ytrain"] = length_ytrain
-        answer["length_ytest"] = length_ytest
+        print(f"Part 1B - The lengths of Xtrain, Xtest, ytrain, ytest are: {len_Xtrain}, {len_Xtest}, {len_Ytrain}, {len_Ytest}")
+        print(f"Part 1B - The maximum value of Xtrain and Xtest is: {max_Xtrain}, {max_Xtest}")
+        answer["len_Xtrain"] = len_Xtrain
+        answer["len_Xtest"] = len_Xtest
+        answer["len_Ytrain"] = len_Ytrain
+        answer["len_Ytest"] = len_Ytest
         answer["max_Xtrain"] = max_Xtrain
         answer["max_Xtest"] = max_Xtest
 
-        # Answer is a dictionary with the same keys as part 1.B
-
-        print(answer)
-        return answer, X, y, Xtest, ytest
+        return answer, Xtrain, ytrain, Xtest, ytest
 
     # --------------------------------------------------------------------------
     """
@@ -202,70 +190,38 @@ class Section3:
         Hint: use the make_scorer function with the average='macro' argument for a multiclass dataset. 
     """
 
-    def partC(
+   def partC(
         self,
         X: NDArray[np.floating],
         y: NDArray[np.int32],
-        Xtest: NDArray[np.floating],
-        ytest: NDArray[np.int32],
-    ) -> dict[str, Any]:
-        """"""
+    ):
+        # Enter your code and fill the `answer` dictionary
+        print("Part 1C")
+        # X, y, Xtest, ytest = u.prepare_data() # preparing initial dataset
+        Xtrain, ytrain = X,y
+        clf = DecisionTreeClassifier(random_state=52) #defining clf and cv
+        cv = KFold(n_splits=5, shuffle = True, random_state = 52)
+        acc_scores1 = u.train_simple_classifier_with_cv(Xtrain=Xtrain, ytrain=ytrain, clf=clf, cv=cv)
+        u.print_cv_result_dict(acc_scores1) 
+        answer = {}
+        answer["clf"] = DecisionTreeClassifier(random_state = 52)  
+        answer["cv"] = KFold(n_splits=5, shuffle=True, random_state = 52)  
+        score_valC={}
+        metrics_score = ['fit_time', 'test_score']
+        for metric in metrics_score:
+            
+            if metric in acc_scores1:
+                # computing mean and standard deviation for the metric
+                mean_value = np.mean(acc_scores1[metric])
+                std_value = np.std(acc_scores1[metric])
+                
+                # updating score_valD dictionary with the computed values
+                score_valC[f'mean_{metric}'] = mean_value
+                score_valC[f'std_{metric}'] = std_value
 
-        
-        print("Part 3C")
-        #X, y, Xtest, ytest = u.prepare_data()
-        Xtrain, ytrain = X, y
-        Xtest, ytest = Xtest, ytest
-
-        skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=52)
-        svc_clf = SVC(random_state=52)
-
-        #scoring metrics
-        scoring = {
-        'accuracy': 'accuracy',
-        'f1_score': make_scorer(f1_score, average='macro'),
-        'precision': make_scorer(precision_score, average='macro'),
-        'recall': make_scorer(recall_score, average='macro')
-        }
-
-        scores = cross_validate(svc_clf, Xtrain, ytrain, scoring=scoring, cv=skf, return_train_score=False)
-
-        scores_3B = {
-        'mean_accuracy': np.mean(scores['test_accuracy']),
-        'std_accuracy': np.std(scores['test_accuracy']),
-        'mean_f1': np.mean(scores['test_f1_score']),
-        'std_f1': np.std(scores['test_f1_score']),
-        'mean_precision': np.mean(scores['test_precision']),
-        'std_precision': np.std(scores['test_precision']),
-        'mean_recall': np.mean(scores['test_recall']),
-        'std_recall': np.std(scores['test_recall'])
-        }
-        
-        #training classifier on training data of imbalanced data 
-        svc_clf.fit(Xtrain, ytrain)
-        train_y_pred = svc_clf.predict(Xtrain)
-        train_confusion_matrix = confusion_matrix(ytrain, train_y_pred)
-        
-        # Predict on the test set and compute the confusion matrix
-        test_y_pred = svc_clf.predict(Xtest)
-        test_confusion_matrix = confusion_matrix(ytest, test_y_pred)
-        
-        # Create the answer dictionary
-        answer = {
-            'scores': scores_3B,
-            'cv': skf,
-            'clf': svc_clf,
-            'confusion_matrix_train': train_confusion_matrix,
-            'confusion_matrix_test': test_confusion_matrix
-        }
-        
-        # seeing if precision is higher than recall
-        precision_higher_than_recall = answer['scores']['mean_precision'] > answer['scores']['mean_recall']
-        answer['is_precision_higher_than_recall'] = precision_higher_than_recall
-        answer['explain_is_precision_higher_than_recall'] = "The model demonstrates greater precision than recall, indicating a stronger capability in accurately identifying true positive cases among all positive predictions. This scenario often occurs in datasets with an uneven class distribution, where the model is better at reducing the occurrence of false positives compared to minimizing false negatives. Essentially, the model is more successful at ensuring the positive predictions it makes are correct, rather than capturing every instance of actual positives"
-        
-        print(answer)
+        answer["scores"] = score_valC
         return answer
+
 
     # --------------------------------------------------------------------------
     """
